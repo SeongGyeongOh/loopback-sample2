660d7b569c9b59eff9e93c89e9a30dd27a1ff255
Gyeong <kamniang@naver.com>
2021-01-17 17:00:59 +0900

    error fixed

--- common/models/major.js
+++ common/models/major.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Major) {
-
+	Major.setMajorByUserId = (major, grade, stNum, minor, req, cb) => {
+		Major.create({
+			major: major,
+			grade: grade,
+			stNum: stNum,
+			minor: minor,
+			userId: req.userInfo.id.toString()
+		})
+		.then(result => {
+			return cb(null, result)
+		})
+		.catch(err => {
+			return cb(err)
+		})
+	}
 };
